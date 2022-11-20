73fef05585abcf6e23e0b7b00b9f2d2a3203111c
etchedprints <etchedprints@gmail.com>
2022-11-20 04:55:40 +0000

    date update

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -25,7 +25,7 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    allMdx {
       nodes {
         frontmatter {
           date
