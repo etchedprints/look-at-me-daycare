9a95b0e4ce25a708e79610b0f2fe8b8c662f0962
etchedprints <etchedprints@gmail.com>
2022-11-20 02:28:10 +0000

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My First Gatsby Site",
+    siteUrl: "https://www.lookatme-daycare.com",
+    title: "Look At Me Daycare",
   },
   plugins: [
     "gatsby-plugin-image",
