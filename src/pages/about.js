8fc5bc5dbe6e1332128037b1d74c58250617b8c3
etchedprints <etchedprints@gmail.com>
2022-11-22 18:29:11 +0000

    updated about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,18 @@ import Seo from '../components/seo'
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
+    <Layout pageTitle="About">
+      <h4>How this project started.</h4>
+      <section>
+        <StaticImage
+          alt="Image of Look At Me Daycare App running on an iPad"
+          src="../images/ipad-running-app.png"
+        />
+        <main>
+          <h2>History</h2>
+          <p>In 2017 my daughter started daycare. I was surprised at how much time went into handwriting notes on all the children's activities during each day. I also thought it was a lot of paper being used for each child. So I started creating this app, and I hope other parents and daycares find it useful. I hope this app can be used for the day to day sharing of all the steps of development and day to day needs within daycares operations.</p>
+        </main>
+      </section>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
