ed46fd484b4b054d31e92dbd0ed3f3c3e6416d73
etchedprints <etchedprints@gmail.com>
2022-11-23 00:15:42 +0000

    home page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,6 +27,65 @@ const IndexPage = () => {
         <button><a href='/downloads/presskit.zip'>DOWNLOAD</a></button>
       </section>
 
+      <section>
+        <aside>
+          <StaticImage
+            alt="Look At Me Daycare running on iPad"
+            src="../images/ipad-running-app.png"
+          />
+        </aside>
+        <main>
+          <section>
+            <h3>Share info as a PDF</h3>
+            <h4><em>The PDF format is an open standard</em></h4>
+            
+            <hr></hr>
+
+            <p>Parents & guardians can download, read, and share that day's events.</p>
+          </section>
+        
+          <section>
+            <h3>Your data stays on your iOS device</h3>
+            <h4><em>Because of rising concern over privacy and data usage, the children database exists on the device</em></h4>
+
+            <hr></hr>
+
+            <p>When the user starts a new day, the database clears past events.</p>
+          </section>
+        </main>
+
+      </section>
+
+      <section>
+        <h1>Videos</h1>
+        <h4>Learn more about Look At Me - Daycare</h4>
+
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/iIUp1Qiqoag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h3>Add A Child</h3>
+          <p>Try for free with one child; add more children with your subscription within the app.</p>
+        </div>
+
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/IDuCmJaITuc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h3>Chart Today's Events</h3>
+          <p>For each child document their activities</p>
+        </div>
+
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/3p8zfWJIHbk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h3>Share</h3>
+          <p>Share each child's developments as a PDF file, as is an attachment to an email or a text message.</p>
+        </div>
+
+        <div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/5j2Tgh_-6CQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h3>Settings</h3>
+          <p>Learn more about the Settings within the app.</p>        
+        </div>
+
+      </section>
+
 
 
     </Layout>
