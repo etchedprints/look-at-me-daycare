b95ebe92b59e45d7ca266cade0829e27427990e5
etchedprints <etchedprints@gmail.com>
2022-11-23 00:22:40 +0000

    added footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -64,6 +64,39 @@ const Layout = ({ pageTitle, children }) => {
         <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
+
+      <footer>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/privacy" className={navLinkText}>
+                Privacy
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/terms" className={navLinkText}>
+                Terms
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/blog" className={navLinkText}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+      
     </div>
   )
 }
